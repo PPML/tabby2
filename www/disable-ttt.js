c4e61e43ef06befa1b7a36d2f0dc265a566fc377
Christian Testa <testa101@gmail.com>
2019-02-12 11:17:27 -0500

    Change f1,f2,f3 to be curried output functions from f in the disable-ttt.js file.

--- www/disable-ttt.js
+++ www/disable-ttt.js
@@ -1,44 +1,34 @@
+// Enable or Disable the Relative Rates for Risk Groups based on Risk Group 
+// 
+// f returns curried functions, i.e. by passing n to f we get back a function 
+// which knows n and will disable/enable the nth TTT panel's relative rates 
+// according to whether or not the risk group is "Define a Custom Risk Group"
+//
+// When a change is made to the risk group in the nth TTT panel, a setInterval 
+// is run so that f(n)() is run every 100 milliseconds. 
+//
+// TODO: Will applying many of these setIntervals slow down the app? What about
+// in slow/old/bad browsers?
+
 $(document).ready(function() {
   
-  f1 = function() {
-    if ($('#ttt1risk')[0][0].text != 'Define a Custom Risk Group') {
-      shinyjs.disable('ttt1progression-rate');
-      shinyjs.disable('ttt1prevalence-rate');
-      shinyjs.disable('ttt1mortality-rate');
-    } else {
-      shinyjs.enable('ttt1progression-rate');
-      shinyjs.enable('ttt1prevalence-rate');
-      shinyjs.enable('ttt1mortality-rate');
-    }
-  };
+	f = function(n) {
+		return function() {
+			if ($('#ttt' + n + 'risk')[0][0].text != 'Define a Custom Risk Group') {
+				shinyjs.disable('ttt' + n + 'progression-rate');
+				shinyjs.disable('ttt' + n + 'prevalence-rate');
+				shinyjs.disable('ttt' + n + 'mortality-rate');
+			} else {
+				shinyjs.enable('ttt' + n + 'progression-rate');
+				shinyjs.enable('ttt' + n + 'prevalence-rate');
+				shinyjs.enable('ttt' + n + 'mortality-rate');
+			}
+		};
+	};
+
   
-  f2 = function() {
-    if ($('#ttt2risk')[0][0].text != 'Define a Custom Risk Group') {
-      shinyjs.disable('ttt2progression-rate');
-      shinyjs.disable('ttt2prevalence-rate');
-      shinyjs.disable('ttt2mortality-rate');
-    } else {
-      shinyjs.enable('ttt2progression-rate');
-      shinyjs.enable('ttt2prevalence-rate');
-      shinyjs.enable('ttt2mortality-rate');
-    }
-  };
-  
-    f3 = function() {
-    if ($('#ttt3risk')[0][0].text != 'Define a Custom Risk Group') {
-      shinyjs.disable('ttt3progression-rate');
-      shinyjs.disable('ttt3prevalence-rate');
-      shinyjs.disable('ttt3mortality-rate');
-    } else {
-      shinyjs.enable('ttt3progression-rate');
-      shinyjs.enable('ttt3prevalence-rate');
-      shinyjs.enable('ttt3mortality-rate');
-    }
-  };
-  
-  
-  $('#ttt1risk').on('change', function() {setInterval(f1, 100)});
-  $('#ttt2risk').on('change', function() {setInterval(f2, 100)});
-  $('#ttt3risk').on('change', function() {setInterval(f3, 100)});
+  $('#ttt1risk').on('change', function() {setInterval(f(1), 100)});
+  $('#ttt2risk').on('change', function() {setInterval(f(2), 100)});
+  $('#ttt3risk').on('change', function() {setInterval(f(3), 100)});
 
 });
