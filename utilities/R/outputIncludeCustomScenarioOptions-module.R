d14f06bb7266ba78c35794785e2fa5e108052106
Christian Testa <testa101@gmail.com>
2019-06-26 10:40:09 -0400

    Add values for custom scenarios

--- utilities/R/outputIncludeCustomScenarioOptions-module.R
+++ utilities/R/outputIncludeCustomScenarioOptions-module.R
@@ -5,12 +5,29 @@ outputIncludeCustomScenarioOptions <- function(input, output, session) {
         id = paste0('tabby1-', estimates$IDs$controls$interventions),
         heading = estimates$interventions$heading,
         labels = c(
-          estimates$interventions$labels# ,
-          # if (input$scenario1Name != '') input$scenario1Name else NULL,
-          # if (input$scenario2Name != '') input$scenario2Name else NULL,
-          # if (input$scenario3Name != '') input$scenario3Name else NULL
+          estimates$interventions$labels,
+          if (input$ttt1name != '') input$ttt1name else NULL,
+          if (input$ttt2name != '') input$ttt2name else NULL,
+          if (input$ttt3name != '') input$ttt3name else NULL,
+          if (input$programChange1Name != '') input$programChange1Name else NULL,
+          if (input$programChange2Name != '') input$programChange2Name else NULL,
+          if (input$programChange3Name != '') input$programChange3Name else NULL,
+          if (input$scenario1Name != '') input$scenario1Name else NULL,
+          if (input$scenario2Name != '') input$scenario2Name else NULL,
+          if (input$scenario3Name != '') input$scenario3Name else NULL
         ),
-        values = estimates$interventions$values
+        values = c(
+				  estimates$interventions$values,
+          if (input$ttt1name != '') 'ttt1' else NULL,
+          if (input$ttt2name != '') 'ttt2' else NULL,
+          if (input$ttt3name != '') 'ttt3' else NULL,
+          if (input$programChange1Name != '') 'programChange1' else NULL,
+          if (input$programChange2Name != '') 'programChange2' else NULL,
+          if (input$programChange3Name != '') 'programChange3' else NULL,
+          if (input$scenario1Name != '') 'scenario1' else NULL,
+          if (input$scenario2Name != '') 'scenario2' else NULL,
+          if (input$scenario3Name != '') 'scenario3' else NULL
+				)
       )
   })
   
@@ -20,12 +37,29 @@ outputIncludeCustomScenarioOptions <- function(input, output, session) {
       id = paste0('tabby1-', trends$IDs$controls$interventions),
       heading = trends$interventions$heading,
       labels = c(
-        estimates$interventions$labels #,
-        # if (input$scenario1Name != '') input$scenario1Name else NULL,
-        # if (input$scenario2Name != '') input$scenario2Name else NULL,
-        # if (input$scenario3Name != '') input$scenario3Name else NULL
+        estimates$interventions$labels,
+				if (input$ttt1name != '') input$ttt1name else NULL,
+				if (input$ttt2name != '') input$ttt2name else NULL,
+				if (input$ttt3name != '') input$ttt3name else NULL,
+				if (input$programChange1Name != '') input$programChange1Name else NULL,
+				if (input$programChange2Name != '') input$programChange2Name else NULL,
+				if (input$programChange3Name != '') input$programChange3Name else NULL,
+        if (input$scenario1Name != '') input$scenario1Name else NULL,
+        if (input$scenario2Name != '') input$scenario2Name else NULL,
+        if (input$scenario3Name != '') input$scenario3Name else NULL
       ),
-      values = trends$interventions$values
+      values = c(
+			  trends$interventions$values,
+				if (input$ttt1name != '') 'ttt1' else NULL,
+				if (input$ttt2name != '') 'ttt2' else NULL,
+				if (input$ttt3name != '') 'ttt3' else NULL,
+				if (input$programChange1Name != '') 'programChange1' else NULL,
+				if (input$programChange2Name != '') 'programChange2' else NULL,
+				if (input$programChange3Name != '') 'programChange3' else NULL,
+				if (input$scenario1Name != '') 'scenario1' else NULL,
+				if (input$scenario2Name != '') 'scenario2' else NULL,
+				if (input$scenario3Name != '') 'scenario3' else NULL
+			)
     )
   })
   
@@ -35,12 +69,29 @@ outputIncludeCustomScenarioOptions <- function(input, output, session) {
       id = paste0('tabby1-', agegroups$IDs$controls$interventions),
       heading = agegroups$interventions$heading,
       labels = c(
-        estimates$interventions$labels #,
-        # if (input$scenario1Name != '') input$scenario1Name else NULL,
-        # if (input$scenario2Name != '') input$scenario2Name else NULL,
-        # if (input$scenario3Name != '') input$scenario3Name else NULL
+        estimates$interventions$labels,
+				if (input$ttt1name != '') input$ttt1name else NULL,
+				if (input$ttt2name != '') input$ttt2name else NULL,
+				if (input$ttt3name != '') input$ttt3name else NULL,
+				if (input$programChange1Name != '') input$programChange1Name else NULL,
+				if (input$programChange2Name != '') input$programChange2Name else NULL,
+				if (input$programChange3Name != '') input$programChange3Name else NULL,
+        if (input$scenario1Name != '') input$scenario1Name else NULL,
+        if (input$scenario2Name != '') input$scenario2Name else NULL,
+        if (input$scenario3Name != '') input$scenario3Name else NULL
       ),
-      values = agegroups$interventions$values
+      values = c(
+			  agegroups$interventions$values,
+				if (input$ttt1name != '') 'ttt1' else NULL,
+				if (input$ttt2name != '') 'ttt2' else NULL,
+				if (input$ttt3name != '') 'ttt3' else NULL,
+				if (input$programChange1Name != '') 'programChange1' else NULL,
+				if (input$programChange2Name != '') 'programChange2' else NULL,
+				if (input$programChange3Name != '') 'programChange3' else NULL,
+				if (input$scenario1Name != '') 'scenario1' else NULL,
+				if (input$scenario2Name != '') 'scenario2' else NULL,
+				if (input$scenario3Name != '') 'scenario3' else NULL
+			)
     )
   })
 }
