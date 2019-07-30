b05dc69ccf139b49c1f14320762b817f618b5882
Christian Testa <testa101@gmail.com>
2019-07-30 16:35:32 -0400

    Turn off program changes for MA

--- server.R
+++ server.R
@@ -161,9 +161,9 @@ shinyServer(function(input, output, session) {
 		# Watch for Updates to Custom Scenarios
 		values <- callModule(updateProgramChanges, NULL, values)
 
-		output$programChange1 <- renderUI({ programChangePanel(1, default_prg_chng() ) })
-		output$programChange2 <- renderUI({ programChangePanel(2, default_prg_chng() ) })
-		output$programChange3 <- renderUI({ programChangePanel(3, default_prg_chng() ) })
+		output$programChange1 <- renderUI({ if (geo_short_code() == 'US') programChangePanel(1, default_prg_chng()) else NULL })
+		output$programChange2 <- renderUI({ if (geo_short_code() == 'US') programChangePanel(2, default_prg_chng()) else NULL })
+		output$programChange3 <- renderUI({ if (geo_short_code() == 'US') programChangePanel(3, default_prg_chng()) else NULL })
 		
 		# Update reactiveValues to reflect selected/defined risk group rate ratios
 		values <- callModule(updateTargetedRiskGroupRates, NULL, risk_group_rate_ratios, values)
