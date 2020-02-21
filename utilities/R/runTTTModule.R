320beae6d088bfb26e0bd841a5fa9da64cdcb832
Christian Testa <testa101@gmail.com>
2020-02-20 19:24:28 -0500

    Fix estimates years numbers used in runTTT/runProgramChanges

--- utilities/R/runTTTModule.R
+++ utilities/R/runTTTModule.R
@@ -121,7 +121,7 @@ runTTT <- function(input, output, session, n, geo_short_code) {
 	new_data[['TRENDS_DATA']] <- restab_big
 
 	new_data[['ESTIMATES_DATA']] <- 
-		dplyr::filter(restab_big, year %in% c(2018, 2020, 2025, 2035, 2049))
+		dplyr::filter(restab_big, year %in% c(2020, 2022, 2025, 2035, 2050))
 
    # cat('new data has: ', as.character(nrow(filter(new_data[['TRENDS_DATA']], scenario == scenario_name))),
    #     'new_data rows\n')
