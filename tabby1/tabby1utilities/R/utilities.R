3d68671b5550f29b97e1dc2803c6c4e82f8eaef7
nswartwo <nswartwo@gmail.com>
2022-07-27 01:22:20 -0400

    update downloads due to officer package update

--- tabby1/tabby1utilities/R/utilities.R
+++ tabby1/tabby1utilities/R/utilities.R
@@ -3,7 +3,7 @@
 #' @importFrom purrr map map_lgl map_df map2 map2_chr pmap flatten_dbl
 #' @importFrom scales muted
 #' @importFrom naturalsort naturalsort
-#' @importFrom officer read_pptx add_slide ph_with_img ph_with_text
+#' @importFrom officer read_pptx add_slide ph_with
 #'
 #'
 #' @docType package
